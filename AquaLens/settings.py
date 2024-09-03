104df5593c3cf6aedcf15ca17e2b34d217ca80a5
Dev-Salem <108498811+Dev-Salem@users.noreply.github.com>
2024-09-03 11:39:27 +0300

    ready to deploy

--- AquaLens/settings.py
+++ AquaLens/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Static root
-STATIC_ROOT = "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 ALLOWED_HOSTS = [
